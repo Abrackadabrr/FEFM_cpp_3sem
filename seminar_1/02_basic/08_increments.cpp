5bd01246b0f69e0386fee2011878d08b2b0003c3
Abrackadabrr <iliynea2002@ya.ru>
2024-09-07 12:38:06 +0300

    Update one

--- seminar_1/02_basic/08_increments.cpp
+++ seminar_1/02_basic/08_increments.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
 int main() {
-    int x = 5, y = 5;
-    x = x++;
-    y = ++y;
+    int x = 5, y = 5, a, b;
+    
+    a = x++;
+    b = ++y;
+
+    // может быть опасно
+    // x = x++;
+    // y = ++y;
+    
     // что будет выведено на экран?
-    std::cout << x << " " << y << std::endl;
+    std::cout << "a = x++ => a = " << a << std::endl;
+    std::cout << "b = ++x => b = " << b << std::endl;
 }
